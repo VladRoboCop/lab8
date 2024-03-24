d0d48a1e33bbe350917ff93841192bb15cf7f919
VladRoboCop <113367165+VladRoboCop@users.noreply.github.com>
2024-03-24 19:35:49 +0200

    Add files via upload

---
+++ Mvc4Demo/Mvc4Demo/Controllers/ProductsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class ProductsController : Controller
+{
+    public IActionResult Index()
+    {
+        var products = new List<Product>()
+        {
+            new Product { ID = 1, Name = "Product 1", Price = 100.50m, CreatedDate = DateTime.Now },
+            new Product { ID = 2, Name = "Product 2", Price = 200.75m, CreatedDate = DateTime.Now },
+            new Product { ID = 3, Name = "Product 3", Price = 300.00m, CreatedDate = DateTime.Now }
+        };
+
+        return View(products);
+    }
+}
---
+++ Mvc4Demo/Mvc4Demo/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class Product
+{
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public DateTime CreatedDate { get; set; }
+}
